1954ed3beeaa85856f6b2ba15c56cd5b648f2871
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-03-23 18:46:26 +0300

    removing ssl

--- src/configs/prisma.js
+++ src/configs/prisma.js
@@ -2,6 +2,6 @@ require("dotenv").config()
 const { PrismaPg } = require("@prisma/adapter-pg")
 const { PrismaClient } = require("../../generated/prisma/client.js")
 const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString, ssl: true });
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 module.exports = prisma;
